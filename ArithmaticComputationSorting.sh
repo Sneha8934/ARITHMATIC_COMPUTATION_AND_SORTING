2fdd462a8f1ac3f311ee68ccbd40a4fb150aa7ba
Sneha8934 <snehamane004@gmail.com>
2020-10-14 13:21:31 +0530

    UC2 Committed

--- ArithmaticComputationSorting.sh
+++ ArithmaticComputationSorting.sh
@@ -5,3 +5,6 @@ echo "--------Welcome To Arithmatic Compution And Sorting ----------"
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+
+result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
