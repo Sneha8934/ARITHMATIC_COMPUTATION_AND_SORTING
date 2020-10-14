5783748a8a9e42d83e38586fdb11ae7b26f06806
Sneha8934 <snehamane004@gmail.com>
2020-10-14 13:37:39 +0530

    UC8 Committed

--- ArithmaticComputationSorting.sh
+++ ArithmaticComputationSorting.sh
@@ -28,3 +28,26 @@ do
   array[index]=${arithmaticOperation[result$((index+1))]}
 done
 echo "${array[@]}"
+
+function descendingOrderSort()
+{
+	for(( index=0; index<${#array[@]}; index ++ ))
+	do
+		for(( indexOne=0; indexOne<${#array[@]}-1; indexOne ++ ))
+		do
+			if (( $(echo "${array[indexOne+1]} > ${array[indexOne]}" | bc -l ) ))
+			then
+				temp=${array[indexOne]}
+				array[indexOne]=${array[indexOne+1]}
+				array[indexOne+1]=$temp
+			fi
+		done
+	done
+	echo "To data descending order"${array[@]}
+}
+
+function main()
+{
+descendingOrderSort
+}
+main
